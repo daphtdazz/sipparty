4b48b17b2d203a3923126bbc2f124c801e1e2805
daphtdazz <daphtdazz@icloud.com>
2015-03-15 18:03:21 +0000

    party-fsm: add transitions

--- sip/party-fsm.py
+++ sip/party-fsm.py
@@ -23,6 +23,14 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
+class FSMError(Exception):
+    pass
+
+
+class UnexpectedInput(FSMError):
+    pass
+
+
 class Timer(object):
 
     def __init__(self):
@@ -56,7 +64,7 @@ class FSM(object):
         # Asynchronous timers are not yet implemented.
         assert not self._fsm_use_async_timers
 
-    def addTransition(self, input, old_state, new_state, action=None,
+    def addTransition(self, old_state, input, new_state, action=None,
                       start_timers=None, stop_timers=None):
         if old_state not in self._fsm_transitions:
             self._fsm_transitions[old_state] = {}
@@ -86,6 +94,13 @@ class FSM(object):
             else:
                 result[key] = []
 
+        # For convenience assume that the first transition set tells us the
+        # initial state.
+        if self._fsm_state is None:
+            self.setState(old_state)
+
+        log.debug("%r: %r -> %r", old_state, input, result[self.KeyNewState])
+
     def setState(self, state):
         if state not in self._fsm_transitions:
             raise ValueError(
@@ -109,18 +124,19 @@ class FSM(object):
         if len(self._fsm_transitions) == 0:
             yield "  (No states or transitions.)"
         for old_state, transitions in self._fsm_transitions.iteritems():
-            yield "  {0!r}".format(old_state)
+            yield "  {0!r}:".format(old_state)
             for input, result in transitions.iteritems():
-                yield "    {0!r} -> {0!r}".format(
+                yield "    {0!r} -> {1!r}".format(
                     input, result[self.KeyNewState])
             yield ""
+        yield "Current state: %r" % self._fsm_state
 
     def __str__(self):
-
         return "\n".join([line for line in self._fsm_strgen()])
 
 if __name__ == "__main__":
     import unittest
+    logging.basicConfig(level=logging.DEBUG)
 
     class TestFSM(unittest.TestCase):
 
@@ -129,8 +145,32 @@ if __name__ == "__main__":
             self.assertEqual(
                 str(nf),
                 "Finite State Machine 'testfsm':\n"
-                "  (No states or transitions.)")
+                "  (No states or transitions.)\n"
+                "Current state: None")
 
-            # nf.addTransition()
+            nf.addTransition("initial", "start", "starting")
+            nf.addTransition("starting", "start", "starting")
+            nf.addTransition("starting", "start_done", "running")
+            nf.addTransition("running", "stop", "stopping")
+            nf.addTransition("stopping", "stop", "stopping")
+            nf.addTransition("stopping", "stop_done", "initial")
+            self.assertEqual(
+                str(nf),
+                "Finite State Machine 'testfsm':\n"
+                "  'stopping':\n"
+                "    'stop_done' -> 'initial'\n"
+                "    'stop' -> 'stopping'\n"
+                "\n"
+                "  'running':\n"
+                "    'stop' -> 'stopping'\n"
+                "\n"
+                "  'initial':\n"
+                "    'start' -> 'starting'\n"
+                "\n"
+                "  'starting':\n"
+                "    'start' -> 'starting'\n"
+                "    'start_done' -> 'running'\n"
+                "\n"
+                "Current state: 'initial'")
 
     unittest.main()
