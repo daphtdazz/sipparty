77554c28d728bd49fa08d899bd71f8c9e8000dfe
daphtdazz <daphtdazz@icloud.com>
2015-12-03 19:20:49 +0000

    ipython: update .gitignore and add initial workbook.

--- sipparty/sip/dialogs/__init__.py
+++ sipparty/sip/dialogs/__init__.py
@@ -14,4 +14,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from six import itervalues
+from ..dialog import Dialog
 from .call import (SimpleCall,)
+
+AllDialogs = [
+    dlg for dlg in itervalues(dict(locals()))
+    if isinstance(dlg, Dialog) and not dlg is Dialog]
+del dlg
+del Dialog
+del itervalues
